6e3f91f34d3b07515afc0dcb8d3979039bd53cd4
leandrocprates <leandro.prates@gmail.com>
2016-06-20 19:04:56 -0300

    remocao de arquivos nao utilizdos

--- WebServiceAngulaJS/target/WebServiceAngulaJS-1.0-SNAPSHOT/resources/cepController.js
+++ WebServiceAngulaJS/target/WebServiceAngulaJS-1.0-SNAPSHOT/resources/cepController.js
@@ -12,10 +12,27 @@ myAppModule.controller("cepController", function ($scope,$http,$sce)
 {
     $scope.person = {firstName:"John Maluco", lastName:"Doe", age:50, eyeColor:"blue"};
     
+    function Endereco() {
+        id=null;
+        rua=null;
+        bairro=null;
+        cidade=null;
+        estado=null;
+        cep=null;
+        msg=null;
+        numero=null;
+        complemento=null;
+    };
+    
+    
+    $scope.endereco = new Endereco(); 
+    
+    $scope.showdiv='BUSCA'; 
     
     $scope.cepDigitado='';
     $scope.endereco;
     $scope.mensagem='';
+    $scope.insercaoMessage='';
 
     
     $scope.buscarEndereco = function(){
@@ -25,13 +42,125 @@ myAppModule.controller("cepController", function ($scope,$http,$sce)
         $http.get(url)
                 .success(function (data){
                     $scope.endereco=data;
+                    $scope.mensagem =null;
                     console.log('Busca correta');
                 })
                 .error(function (data){
                     console.log('Erro na busca ');
                     $scope.mensagem = data.msg; 
+                    $scope.endereco=null;
                 });                
     };    
+    
+    
+    $scope.inserirEndereco = function(){
+        
+        var url='http://localhost:8084/WebServiceAngulaJS/rest/buscaEndereco/addEndereco' ;
+        
+        
+        if ( validaComposObrigatorio() == false) {
+            return;
+        }
+        
+        
+        $http.post(url,$scope.endereco)
+                .success(function (data){
+                    console.log('Endereco adicionado com sucesso.');
+                    $scope.insercaoMessage='Endereco Inserido com sucesso.';
+                    $scope.insercaoMessageErro='';
+                })
+                .error(function (data){
+                    console.log('Erro ao adicionar endereco.');
+                });
+        
+    };
+    
+    validaComposObrigatorio=function (){
+        
+        if ( ( angular.isUndefined($scope.endereco.rua) || $scope.endereco.rua == null )
+                ||  ( angular.isUndefined($scope.endereco.numero) || $scope.endereco.numero == null )
+                ||  ( angular.isUndefined($scope.endereco.cep) || $scope.endereco.cep == null )
+                ||  ( angular.isUndefined($scope.endereco.cidade) || $scope.endereco.cidade == null )
+                ||  ( angular.isUndefined($scope.endereco.estado) || $scope.endereco.estado == null )
+                
+            ) {
+            
+            $scope.insercaoMessageErro = 'Campos Rua/Cep/Numero/Cidade/Estado sao obrigatorios'; 
+            return false;
+        }
+        return true;
+        
+    };
+    
+    
+    $scope.alterarEndereco= function(endereco){
+        $scope.endereco= endereco;
+        $scope.showdiv='INSERIR';
+    };
+    
+    $scope.excluir= function(endereco){
+        $scope.endereco= endereco;
+        
+        var url='http://localhost:8084/WebServiceAngulaJS/rest/buscaEndereco/excluirEndereco' ;
+        
+        var req = {
+         method: 'DELETE',
+         url: url,
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         data: $scope.endereco
+        };
+        
+        $http(req)
+            .success(function (data){
+                console.log('Endereco deletado com sucesso.');
+                $scope.endereco = new Endereco(); 
+                $scope.listarEnderecos();
+            })
+            .error(function (data){
+                console.log('Erro ao deletar endereco.');
+            });
+        
+        
+        
+    };
+    
+    
+    
+    
+    
+    
+    $scope.listarEnderecos = function(){
+        
+        var url='http://localhost:8084/WebServiceAngulaJS/rest/buscaEndereco/listarEnderecos' ;
+        
+        $http.get(url)
+                .success(function (data){
+                    $scope.listaEndereco= ( data.length == 1 )?  new Array(data): data ;
+                    console.log('Busca correta');
+                })
+                .error(function (data){
+                    console.log('Erro na busca ');
+                });                
+    };
+    
+    $scope.irpara= function(irpara){
+        $scope.showdiv=irpara; 
+        
+        $scope.insercaoMessageErro=null;
+        $scope.insercaoMessage=null;
+        
+        if ($scope.showdiv == 'LISTAR' ){
+            $scope.listarEnderecos();
+        } else if ($scope.showdiv == 'INSERIR' ){
+            $scope.endereco = new Endereco();    
+        }
+        
+        
+    };
+    
+    /*
     
     
     
@@ -93,7 +222,7 @@ myAppModule.controller("cepController", function ($scope,$http,$sce)
                 });
     };
     
-    
+    */
 
     
     
